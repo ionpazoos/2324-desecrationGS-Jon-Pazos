867ba426e4ccbadbd049d84b7bd6b31a79f8c5ef
ionpazoos <jon.pazos@ikasle.aeg.eus>
2024-06-05 11:09:41 +0200

    Creamos calculos de daño

--- Characters/SuperHero.mjs
+++ Characters/SuperHero.mjs
@@ -1,7 +1,46 @@
-import Character from "./Character.mjs";
-
+import { Dies } from "../Constants.mjs";
 export default class SuperHero extends Character {
   constructor(id, name, slug, intelligence, strength, speed, durability, power, combat) {
     super(id, name, slug, intelligence, strength, speed, durability, power, combat);
   }
+
+  attack(dieResult,dies) {
+    let damage;
+    if (dieResult === 1) {
+      // Pifia
+      damage = this.SPD / dieResult;
+      console.log( this.name + ' ha hecho una pifia de ' + damage);
+    } else if (dieResult === 2) {
+      // Pifia
+      damage = this.SPD / (4 * dieResult);
+      console.log( this.name + ' ha hecho una pifia de ' + damage);
+    } else if (dieResult >= 3 && dieResult <= 17) {
+      // Daño normal
+      damage = (this.POW + this.STR) * dieResult / 100;
+      console.log( this.name + ' ha inflingido un daño de ' + damage);
+    } else if (dieResult >= 18 && dieResult <= 20) {
+      // Crítico
+      damage = this.calculateCriticalDamage(dieResult) + (this.POW + this.STR) * dieResult / 100;
+      console.log( this.name + ' ha hecho un daño critico de ' + damage);
+    }
+    return damage;
+  }
+
+  calculateCriticalDamage(dieResult,dies) {
+    let damage;
+    switch (dieResult) {
+      case 18:
+        damage = (this.INT * this.DUR) / 100 * dies[Dies.D3].roll();
+        break;
+      case 19:
+        damage = (this.INT * this.DUR) / 100 * (dies[Dies.D3].roll() + dies[Dies.D3].roll());
+        break;
+      case 20:
+        damage = (this.INT * this.DUR) / 100 * (dies[Dies.D3].roll() + dies[Dies.D3].roll() + dies[Dies.D3].roll());
+        break;
+    }
+    return damage;
+  }
+
+
 }
--- Characters/SuperVillain.mjs
+++ Characters/SuperVillain.mjs
@@ -1,7 +1,37 @@
-import Character from "./Character.mjs";
-
+import { Dies } from "../Constants.mjs";
 export default class SuperVillain extends Character {
   constructor(id, name, slug, intelligence, strength, speed, durability, power, combat) {
     super(id, name, slug, intelligence, strength, speed, durability, power, combat);
   }
+
+  attack(dieResult) {
+    let damage;
+    if (dieResult >= 1 && dieResult <= 17) {
+      // Normal
+      damage = (this.POW + this.STR) * dieResult / 100;
+      console.log( this.name + ' ha hecho un daño de ' + damage);
+    } else if (dieResult >= 18 && dieResult <= 20) {
+      // Crítico
+      damage = this.calculateCriticalDamage(dieResult) + (this.POW + this.STR) * dieResult / 100;
+      console.log( this.name + ' ha hecho un daño critico de ' + damage);
+    }
+    return damage;
+  }
+
+  calculateCriticalDamage(dieResult,dies) {
+    let damage;
+    switch (dieResult) {
+      case 18:
+        damage = (this.INT * this.DUR) / 100 * (dies[Dies.D3].roll() + dies[Dies.D3].roll());
+        break;
+      case 19:
+        damage = (this.INT * this.DUR) / 100 * (dies[Dies.D3].roll() + dies[Dies.D3].roll() + dies[Dies.D3].roll());
+        break;
+      case 20:
+        damage = (this.INT * this.DUR) / 100 * (dies[Dies.D5].roll() + dies[Dies.D5].roll() + dies[Dies.D5].roll() + dies[Dies.D5].roll());
+        break;
+    }
+    return damage;
+  }
+
 }
