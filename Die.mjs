e9ea332d5de71b645dcc22e63cc54cd9f3ad83ca
ionpazoos <jon.pazos@ikasle.aeg.eus>
2024-06-05 10:00:31 +0200

    Creamos clase die con la factoria

---
+++ Die.mjs
@@ -0,0 +1,52 @@
+export default class Die {
+    constructor(values) {
+      this.values = values;
+    }
+    static createDie(type) {
+      let values;
+      switch (type) {
+        case "D3":
+          values = [1, 2, 3];
+          break;
+        case "D5":
+          values = [1, 2, 3, 4, 5];
+          break;
+        case "D20":
+          values = [];
+          for (let i = 0; i < 20; i++) {
+            values.push(i);
+          }
+          break;
+        case "D100":
+            values = [];
+            for (let i = 0; i < 100; i++) {
+              values.push(i);
+            }
+          break;
+      }
+      return new Die(values);
+    }
+
+    roll() {
+        const randomIndex = Math.floor(Math.random() * this.values.length);
+        return this.values[randomIndex];
+      }
+
+    createD3() {
+    return Die.createDie("D3");
+    }
+    
+    createD5() {
+    return Die.createDie("D5");
+    }
+    
+    createD20() {
+    return Die.createDie("D20");
+    }
+    
+    createD100() {
+    return Die.createDie("D100");
+    }
+      
+  }
+  
